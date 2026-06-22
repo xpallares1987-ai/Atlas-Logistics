23d56dd35db8db3a6713fa3f38a4d03d3f370b74
ESXPA <x.pallares1987@gmail.com>
2026-06-22 03:48:56 +0200

    chore: fix eslint warnings and typings

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ export default tseslint.config(
       },
     },
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": ["warn"],
       "no-console": "off",
