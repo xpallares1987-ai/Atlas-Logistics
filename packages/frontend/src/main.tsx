71189756c7c71f2d9e83526cb813629e9de2d595
ESXPA <x.pallares1987@gmail.com>
2026-07-05 14:11:23 +0200

    Save uncommitted changes

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { FirebaseProvider } from '@atlas/ui';
 import App from './App';
 import './index.css';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { registerSW } from 'virtual:pwa-register';
 
@@ -12,12 +14,23 @@ registerSW({ immediate: true });
 
 const queryClient = new QueryClient();
 
+const firebaseConfig = {
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <FirebaseProvider config={firebaseConfig} databaseId="database01">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </FirebaseProvider>
   </React.StrictMode>,
 );
